c68ed025f872f9adbe609dfdf9e1b540f1c02a90
Utkarsh <utkarsh.vishwakarma@genuinemark.org>
2024-01-25 13:28:06 +0530

    minor fix

--- db/knexfile.js
+++ db/knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'postgres',
+      database: 'ticketing',
       user:     'genefied',
       password: '123456'
     },
